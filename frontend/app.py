88b370e2bbcd1512ceb3772de1288d41de75bdfd
Santiago Moyano <santiago98moy@gmail.com>
2021-03-23 20:30:51 -0300

    modifique archivo app.py en frontend y backend

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import os
-from dotenv import load_dotenv
-load_dotenv()
-app = Flask(__name__)
+
+app = create_app()
+app.app_context().push()
+
 if __name__ == '__main__':
     app.run(debug = True, port = os.getenv('PORT'))
