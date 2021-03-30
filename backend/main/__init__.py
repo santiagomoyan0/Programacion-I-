2bac19759bed76fb31a362cbd5e565a939c896a4
Santiago Moyano <santiago98moy@gmail.com>
2021-03-30 20:32:13 -0300

    añadi carpeta resources y agregue archivos bolsones e init.py en ella

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_restful import  Api
+import main.resources as resources
+api = Api()
 
 def create_app():
     app = Flask(__name__)
---
+++ backend/main/resources/bolsones.py
@@ -0,0 +1,5 @@
+from flask_restful import Resource
+from flask import request
+
+class Bolsones():
+    
