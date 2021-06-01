b4d31fb1db6f201c69fe6957c76c62e3747d9be7
Santiago Moyano <santiago98moy@gmail.com>
2021-06-01 19:29:58 -0300

    arreglos para tp 6

--- backend/main/mail/functions.py
+++ backend/main/mail/functions.py
@@ -1,8 +1,9 @@
 from .. import mailsender
-from flask import current_app, render_template
+from flask import current_app, render_template, Blueprint
 from flask_mail import Message
 from smtplib import SMTPException
-
+from main.models import UsuarioModel, BolsonModel
+from main.auth.decoradores import admin_required
 def sendMail(to, subject, template, **kwargs):
     #Configuracion del mail
     msg = Message( subject, sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=to)
@@ -19,6 +20,15 @@ def sendMail(to, subject, template, **kwargs):
 
 mail = Blueprint('mail', __name__, url_prefix='/mail')
 
-@mail.promo
-def sendPromo(to, subject, template, **kwargs):
-    msg = Message()
+@mail.route('/promocion', methods=['POST'])
+@admin_required
+def promocion():
+    usuarios = db.session.query(UsuarioModel).filter(UsuarioModel.role == 'cliente').all()
+    bolsonesVenta = db.session.query(BolsonModel).filter(BolsonModel.aprobado == 1).all()
+    try:
+        for usuario in usuarios:
+            sent = sendMail([usuario.mail], "Ofertas de la semana", 'promo', usuario = usuario, bolsones = [bolson.nombre for bolson in bolsonesVenta])
+    except SMTPException as e:
+        print(str(e))
+        return "Mail deliver failed"
+    return 'mails sent', 200
